CalyxOS/vendor_calyx

envsetup: Fix breakfast
Change-Id: I9742638765f323876bffcdd0d9c1131d28a54614

build/envsetup.sh
@@ -39,6 +39,8 @@ function breakfast()
     target=$1
     local variant=$2
 
+    source "${ANDROID_BUILD_TOP}/calyx/scripts/vars/common"
+
     if [ $# -eq 0 ]; then
         # No arguments, so let's have the full menu
         lunch
@@ -52,7 +54,7 @@ function breakfast()
                 variant="userdebug"
             fi
 
-            lunch calyx_$target-$variant
+            lunch calyx_$target-$aosp_target_release-$variant
         fi
     fi
     return $?

rayavanindra/vendor_pixys

Signed-off-by: aswin7469 <aswinas@pixysos.com>

build/envsetup.sh
@@ -69,6 +69,7 @@ function breakfast()
 {
     target=$1
     local variant=$2
+    local aosp_target_release=$(cat ${ANDROID_BUILD_TOP}/vendor/pixys/vars/aosp_target_release 2>/dev/null)
 
     if [ $# -eq 0 ]; then
         # No arguments, so let's have the full menu
@@ -83,7 +84,7 @@ function breakfast()
                 variant="userdebug"
             fi
 
-            lunch pixys_$target-$variant
+            lunch pixys_$target-$aosp_target_release-$variant
         fi
     fi
     return $?